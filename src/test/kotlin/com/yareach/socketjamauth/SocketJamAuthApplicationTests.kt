d1d4c35d25d4545bd60939bf7711c654aee1e396
yareach8345 <mine8345@naver.com>
2025-09-07 17:25:00 +0900

    init - socket-jam-auth 프로젝트 생성

---
+++ src/test/kotlin/com/yareach/socketjamauth/SocketJamAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yareach.socketjamauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocketJamAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
